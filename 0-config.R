21468727a0808f356a104687247c2b51c15c159d
geneh0 <52678798+geneh0@users.noreply.github.com>
2021-01-26 23:26:52 -0800

    bangladesh primary analyses

--- 0-config.R
+++ 0-config.R
@@ -61,4 +61,8 @@ tableau10 <- c("#1F77B4","#FF7F0E","#2CA02C","#D62728",
                "#9467BD","#8C564B","#E377C2","#7F7F7F",
                "#BCBD22","#17BECF")
 
+if(!require(faraway)){
+  install.packages("faraway")
+  library(faraway)
+}
 
